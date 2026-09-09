7a7f8f29a2a07f16485442a2bf41431c0b1e2860
irfanuddinahmad <34648393+irfanuddinahmad@users.noreply.github.com>
2026-09-09 14:07:37 +0500

    fix: remove unnecessary try/except around __version__ (not present pre-migration)

--- src/edx_event_bus_redis/__init__.py
+++ src/edx_event_bus_redis/__init__.py
@@ -2,13 +2,9 @@
 Redis Streams implementation for the Open edX event bus.
 """
 
-from importlib.metadata import PackageNotFoundError
 from importlib.metadata import version as get_version
 
 from edx_event_bus_redis.internal.consumer import RedisEventConsumer
 from edx_event_bus_redis.internal.producer import create_producer
 
-try:
-    __version__ = get_version("edx-event-bus-redis")
-except PackageNotFoundError:  # pragma: no cover
-    pass
+__version__ = get_version("edx-event-bus-redis")
